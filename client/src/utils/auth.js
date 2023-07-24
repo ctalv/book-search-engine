e3411061e827f4985fa9831addb51d1c922752b7
ctalv <122413805+ctalv@users.noreply.github.com>
2023-07-24 12:56:43 -0500

    fixed client auth.js; added pseudocode to server auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,91 +1,49 @@
-// Update the auth middleware function to work with the GraphQL API
-// pulled from MERN Activities 25 auth.js
+// use this to decode a token and get the user's information out of it
+import decode from 'jwt-decode';
 
-const jwt = require('jsonwebtoken');
+// create a new class to instantiate for a user
+class AuthService {
+  // get user data
+  getProfile() {
+    return decode(this.getToken());
+  }
 
-const secret = 'mysecretssshhhhhhh';
-const expiration = '2h';
+  // check if user's logged in
+  loggedIn() {
+    // Checks if there is a saved token and it's still valid
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token); // handwaiving here
+  }
 
-module.exports = {
-  authMiddleware: function ({ req }) {
-    // allows token to be sent via req.body, req.query, or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
-
-    // We split the token string into an array and return actual token
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
-
-    if (!token) {
-      return req;
-    }
-
-    // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
+  // check if token is expired
+  isTokenExpired(token) {
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
+      const decoded = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
     }
+  }
 
-    // return the request object so it can be passed to the resolver as `context`
-    return req;
-  },
-  signToken: function ({ email, name, _id }) {
-    const payload = { email, name, _id };
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
-};
+  getToken() {
+    // Retrieves the user token from localStorage
+    return localStorage.getItem('id_token');
+  }
 
+  login(idToken) {
+    // Saves user token to localStorage
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
 
+  logout() {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem('id_token');
+    // this will reload the page and reset the state of the application
+    window.location.assign('/');
+  }
+}
 
-// OLD CODE
-// // use this to decode a token and get the user's information out of it
-// import decode from 'jwt-decode';
-
-// // create a new class to instantiate for a user
-// class AuthService {
-//   // get user data
-//   getProfile() {
-//     return decode(this.getToken());
-//   }
-
-//   // check if user's logged in
-//   loggedIn() {
-//     // Checks if there is a saved token and it's still valid
-//     const token = this.getToken();
-//     return !!token && !this.isTokenExpired(token); // handwaiving here
-//   }
-
-//   // check if token is expired
-//   isTokenExpired(token) {
-//     try {
-//       const decoded = decode(token);
-//       if (decoded.exp < Date.now() / 1000) {
-//         return true;
-//       } else return false;
-//     } catch (err) {
-//       return false;
-//     }
-//   }
-
-//   getToken() {
-//     // Retrieves the user token from localStorage
-//     return localStorage.getItem('id_token');
-//   }
-
-//   login(idToken) {
-//     // Saves user token to localStorage
-//     localStorage.setItem('id_token', idToken);
-//     window.location.assign('/');
-//   }
-
-//   logout() {
-//     // Clear user token and profile data from localStorage
-//     localStorage.removeItem('id_token');
-//     // this will reload the page and reset the state of the application
-//     window.location.assign('/');
-//   }
-// }
-
-// export default new AuthService();
+export default new AuthService();
