cab13775875c05b9d61149c15b9f9ed29c536058
ctalv <122413805+ctalv@users.noreply.github.com>
2023-07-24 13:27:17 -0500

    added base resolver content

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,57 @@
 // Define the query and mutation functionality to work with the Mongoose models.
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        users: async () => {
+            return User.find().populate('books');
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username }).populate('books');
+        },
+        books: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Book.find(params).sort({ createdAt: -1 });
+        },
+        book: async (parent, { bookId }) => {
+            return Book.findOne({ _id: bookId });
+        },
+    },
+
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, { thoughtText, thoughtAuthor }) => {
+            const thought = await Book.create({ thoughtText, thoughtAuthor });
+
+            await User.findOneAndUpdate(
+                { username: thoughtAuthor },
+                { $addToSet: { thoughts: thought._id } }
+            );
+
+            return book;
+        },
+        deleteBook: async (parent, { bookId }) => {
+            return Book.findOneAndDelete({ _id: bookId });
+        },
+    },
+};
+
+module.exports = resolvers;
