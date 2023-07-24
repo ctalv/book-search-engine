f365abc8493ee03cd9dfc6f7cb404f8c63b8f47b
ctalv <122413805+ctalv@users.noreply.github.com>
2023-07-24 13:05:14 -0500

    added server requirements

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 // Implement the Apollo Server and apply it to the Express server as middleware.
-
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
+
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes'); we wont use routes
+const { typeDefs, resolvers } = require('./schemas');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
