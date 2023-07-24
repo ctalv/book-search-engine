5fe1ea8de6802d4c56304a42b5f54bea7aae3fa4
ctalv <122413805+ctalv@users.noreply.github.com>
2023-07-24 16:46:26 -0500

    messed with mutations.js and queries.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,46 @@
+import { gql } from '@apollo/client';
+
 // LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
 // ADD_USER will execute the addUser mutation.
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
 // SAVE_BOOK will execute the saveBook mutation.
+export const SAVE_BOOK = gql`
+  mutation saveBook($userId: ID!, $commentText: String!) {
+    saveBook(userId: $userId, savedBooks: $commentText) {
+      _id
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
 
 // REMOVE_BOOK will execute the removeBook mutation
